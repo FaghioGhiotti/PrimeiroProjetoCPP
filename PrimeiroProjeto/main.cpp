22e475e1126078c61e55cd9690389f65c2bffcfe
FaghioGhiotti <fabioghiotti2@gmail.com>
2021-05-15 20:34:44 -0300

    comitando primeiro código

---
+++ PrimeiroProjeto/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numeroUsuario;
+
+    cout << "Digite o numero:" << endl;
+    cin >> numeroUsuario;
+    cout << "O numero informado foi: " << numeroUsuario <<endl;
+    return 0;
+}
---
+++ SegundoProjeto/SegundoProjeto/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int primeiroValor;
+    int segundoValor;
+    int valorSoma;
+
+    cout << "Programa para somar dois valores" << endl << endl << endl;
+
+    cout << "Digite primeiro valor ";
+    cin >> primeiroValor;
+
+    cout << "o valor e : " << primeiroValor << endl;
+
+    cout << "Digite segundo valor e: ";
+    cin >> segundoValor;
+
+    cout << "o segundo valor e : " << primeiroValor << endl;
+
+    valorSoma = (primeiroValor + segundoValor);
+
+    cout << "a soma e " << valorSoma;
+
+    return 0;
+}
